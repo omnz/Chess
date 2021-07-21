c8c74dd0b5e1adfb1d2b8b0599c3d60ef921029b
Omar N <54334118+omnz@users.noreply.github.com>
2021-07-21 14:58:18 -0400

    Update board.py

--- board.py
+++ board.py
@@ -1,3 +1,5 @@
+from player import Player
+
 class Board:
     """Chess board model"""
     
@@ -7,25 +9,41 @@ class Board:
         self.cols = 8
         self.board = [[0]*self.cols for _ in range(0, self.rows)]
     
-    def setup(self):
+    def setup(self, white, black):
         """Setup the initial board state"""
-        for x in range(0, self.cols):
-            if(x == 0 or x == 7):
-                self.board[x][0] = "R"
-                self.board[x][1] = "N"
-                self.board[x][2] = "B"
-                self.board[x][3] = "Q"
-                self.board[x][4] = "K"
-                self.board[x][5] = "B"
-                self.board[x][6] = "N"
-                self.board[x][7] = "R"
-            if(x == 1 or x == 6):
+        for x in range(0, self.rows):
+            # Black pieces - Back row
+            if x == 0:
+                middle_piece = (int)(len(black.pieces)/2)
+
                 for y in range(0, self.cols):
-                    self.board[x][y] = "P"
+                    self.board[x][y] = black.pieces[middle_piece]
+                    middle_piece += 1
+            
+            # Black pieces - Front row
+            if x == 1:
+                for y in range(0, self.cols):
+                    self.board[x][y] = black.pieces[y]
+            
+            # White pieces - Front row
+            if x == 6:
+                for y in range(0, self.cols):
+                    self.board[x][y] = white.pieces[y]
+
+            # White pieces - Back row
+            if x == 7:
+                middle_piece = (int)(len(white.pieces)/2)
+
+                for y in range(0, self.cols):
+                    self.board[x][y] = white.pieces[middle_piece]
+                    middle_piece += 1
 
     def display(self):
         """Print out the current state of the board"""
         for row in self.board:
-            for x in row:
-                print(f" {x} ", end='')
+            for piece in row:
+                if piece == 0:
+                    print(f" {piece} ", end='')
+                else:
+                    print(f" {piece.display} ", end='')
             print()
