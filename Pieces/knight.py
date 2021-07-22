d33c40fb35a3f9497bb1bf4b7b122dd1f18aa90a
Omar N <54334118+omnz@users.noreply.github.com>
2021-07-22 13:35:49 -0400

    Update knight.py

--- Pieces/knight.py
+++ Pieces/knight.py
@@ -1,8 +1,11 @@
-class Knight:
-    """Pawn piece"""
+from Pieces.piece import Piece
+
+
+class Knight(Piece):
+    """Knight piece"""
 
     def __init__(self):
         """Initialize piece attributes"""
+        super().__init__()
         self.display = 'N'
     # def move():
-    # def canMove():
