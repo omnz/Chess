88c130c0d20b169d672ae0a63cb36acfdf9a879a
Omar N <54334118+omnz@users.noreply.github.com>
2021-07-22 13:35:54 -0400

    Update bishop.py

--- Pieces/bishop.py
+++ Pieces/bishop.py
@@ -1,8 +1,11 @@
-class Bishop:
-    """Pawn piece"""
+from Pieces.piece import Piece
+
+
+class Bishop(Piece):
+    """Bishop piece"""
 
     def __init__(self):
         """Initialize piece attributes"""
+        super().__init__()
         self.display = 'B'
     # def move():
-    # def canMove():
