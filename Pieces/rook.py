325000eb344c1a8dad2e65a13bc749e7e3b60d89
Omar N <54334118+omnz@users.noreply.github.com>
2021-07-22 13:35:38 -0400

    Update rook.py

--- Pieces/rook.py
+++ Pieces/rook.py
@@ -1,8 +1,11 @@
-class Rook:
-    """Pawn piece"""
+from Pieces.piece import Piece
+
+
+class Rook(Piece):
+    """Rook piece"""
 
     def __init__(self):
         """Initialize piece attributes"""
+        super().__init__()
         self.display = 'R'
     # def move():
-    # def canMove():
