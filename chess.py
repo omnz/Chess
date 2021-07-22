d2b4e4478aad10af36aa03942106503ccf8cae06
Omar N <54334118+omnz@users.noreply.github.com>
2021-07-22 13:35:57 -0400

    Update chess.py

--- chess.py
+++ chess.py
@@ -6,6 +6,10 @@ p1 = Player('Player 1')
 p2 = Player('Player 2')
 board = Board()
 
+turn_order = []
+game_over = False
+winner = ''
+
 # Ask first player for piece color
 while True:
     color = input(f"{p1.get_name()}, which color piece are you (white/black)? ")
@@ -26,10 +30,28 @@ while True:
     else:
         print("Error: Did not enter white or black!\n")
 
+print()
+
 # Setup the board
 if(p1.get_piece_color() == 'white'):
     board.setup(p1, p2)
+    p1.turn()
+
+    turn_order.append(p1)
+    turn_order.append(p2)
 else:
     board.setup(p2, p1)
+    p2.turn()
+    
+    turn_order.append(p2)
+    turn_order.append(p1)
 
 board.display()
+# print(p1.pieces[0].can_move)
+# print(p2.pieces[0].can_move)
+
+# Game Loop
+# while game_over == False:
+#     for player in turn_order:
+#         while True:
+#             piece_to_move = input("Enter coordinates of piece you wish to move: ")
