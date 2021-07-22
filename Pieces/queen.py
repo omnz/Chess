45ee7198c3664005ae3388800da28d878615683a
Omar N <54334118+omnz@users.noreply.github.com>
2021-07-22 13:35:41 -0400

    Update queen.py

--- Pieces/queen.py
+++ Pieces/queen.py
@@ -1,8 +1,11 @@
-class Queen:
-    """Pawn piece"""
+from Pieces.piece import Piece
+
+
+class Queen(Piece):
+    """Queen piece"""
 
     def __init__(self):
         """Initialize piece attributes"""
+        super().__init__()
         self.display = 'Q'
     # def move():
-    # def canMove():
