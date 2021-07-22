2d4543c09c4e0b7b4eca00a0a0b09fce4c8e969e
Omar N <54334118+omnz@users.noreply.github.com>
2021-07-22 13:35:51 -0400

    Update king.py

--- Pieces/king.py
+++ Pieces/king.py
@@ -1,8 +1,11 @@
-class King:
-    """Pawn piece"""
+from Pieces.piece import Piece
+
+
+class King(Piece):
+    """King piece"""
 
     def __init__(self):
         """Initialize piece attributes"""
+        super().__init__()
         self.display = 'K'
     # def move():
-    # def canMove():
