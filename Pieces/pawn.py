312f198e5967c58c4b270e80846f4433e421baf4
Omar N <54334118+omnz@users.noreply.github.com>
2021-07-22 13:35:46 -0400

    Update pawn.py

--- Pieces/pawn.py
+++ Pieces/pawn.py
@@ -1,8 +1,11 @@
-class Pawn:
+from Pieces.piece import Piece
+
+
+class Pawn(Piece):
     """Pawn piece"""
 
     def __init__(self):
         """Initialize piece attributes"""
+        super().__init__()
         self.display = 'P'
     # def move():
-    # def canMove():
