a50b7ef88a3fbde289587b79b26c38a991b26cc8
Omar N <54334118+omnz@users.noreply.github.com>
2021-08-02 17:38:52 -0400

    Update pawn.py

--- Pieces/pawn.py
+++ Pieces/pawn.py
@@ -9,3 +9,17 @@ class Pawn(Piece):
         super().__init__()
         self.display = 'P'
     # def move():
+
+    def check_position(self, board, position):
+        row = position[0]
+        col = position[1]
+        board_pos = board.board[row][col]
+        piece = board_pos['piece']['piece']
+
+        print(self.get_position())
+        print(piece.get_position())
+
+        if(self.get_color() == 'white'):
+            if(self.get_position()[0] < piece.get_position()[0]):
+                return 0
+        return self
