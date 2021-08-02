93d29e747646f4f3dc53f37aee55f1eb879fd513
Omar N <54334118+omnz@users.noreply.github.com>
2021-08-02 17:38:59 -0400

    Update piece.py

--- Pieces/piece.py
+++ Pieces/piece.py
@@ -1,7 +1,21 @@
 class Piece:
     def __init__(self):
         self.can_move = False
+        self.position = None
+        self.color = None
     
     def flip_can_move(self):
         """Switch whether the piece can move or not"""
         self.can_move = not self.can_move
+
+    def get_position(self):
+        return self.position
+    
+    def set_position(self, row, col):
+        self.position = (row, col)
+
+    def set_color(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
