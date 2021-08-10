903562d3b6817ff94e61f36d08e9f216a995befc
Omar N <54334118+omnz@users.noreply.github.com>
2021-08-10 15:17:20 -0400

    Update bishop.py

--- Pieces/bishop.py
+++ Pieces/bishop.py
@@ -1,4 +1,5 @@
 from Pieces.piece import Piece
+from Pieces.empty import Empty
 
 
 class Bishop(Piece):
@@ -8,4 +9,71 @@ class Bishop(Piece):
         """Initialize piece attributes"""
         super().__init__()
         self.display = 'B'
-    # def move():
+    
+    def check_position(self, board):
+        """Returns '1' if position is valid."""
+        
+        possible_positions = []
+
+        for count in range(0, 4):
+            # Reset row and col
+            row = self.get_position()[0]
+            col = self.get_position()[1]
+
+            # Check top-left
+            if count == 0:
+                while row > 0 or col > 0:
+                    row = row - 1
+                    col = col - 1
+                    
+                    try:
+                        board_pos = board.board[row][col]
+
+                        if isinstance(board_pos['piece'], Empty):
+                            possible_positions.append(board_pos)
+                    except:
+                        continue
+
+            # Check top-right
+            elif count == 1:
+                while row > 0 or col < board.cols:
+                    row = row - 1
+                    col = col + 1
+
+                    try:
+                        board_pos = board.board[row][col]
+
+                        if isinstance(board_pos['piece'], Empty):
+                            possible_positions.append(board_pos)
+                    except:
+                        continue
+
+            # Check bottom-right
+            elif count == 2:
+                while row < board.rows or col < board.cols:
+                    row = row + 1
+                    col = col + 1
+
+                    try:
+                        board_pos = board.board[row][col]
+
+                        if isinstance(board_pos['piece'], Empty):
+                            possible_positions.append(board_pos)
+                    except:
+                        continue
+
+            # Check bottom-left
+            elif count == 3:
+                while row < board.rows or col > 0:
+                    row = row + 1
+                    col = col - 1
+
+                    try:
+                        board_pos = board.board[row][col]
+
+                        if isinstance(board_pos['piece'], Empty):
+                            possible_positions.append(board_pos)
+                    except:
+                        continue
+
+        return possible_positions
