9c8d3f605a1602c11d6898d4ba601f9bdd52cad6
Omar N <54334118+omnz@users.noreply.github.com>
2021-08-11 17:09:10 -0400

    Update board.py

--- board.py
+++ board.py
@@ -1,6 +1,7 @@
 import pygame
 from player import Player
 from Pieces.empty import Empty
+from Pieces.queen import Queen
 import pygame
 
 class Board:
@@ -69,43 +70,69 @@ class Board:
                     self.board[x][y]['piece'] = Empty()
                     self.board[x][y]['piece'].set_position(x, y)
     
-    def move_piece(self, screen, last_clicked, new_click):
+    def move_piece(self, player, last_clicked, new_click, p1, p2):
         """Move a piece and update relevant information"""
-        # print(last_clicked)
-        # print(new_click)
+
         temp_pos = last_clicked['rect_pos']
         last_clicked['rect_pos'].update(new_click['rect_pos'])
         new_click['rect_pos'].update(temp_pos)
 
-        # last_clicked['rect_pos'].move(new_click['rect_pos'][0], new_click['rect_pos'][1])
-        # new_click['rect_pos'].move(temp_pos[0], temp_pos[1])
+        last_position_x = last_clicked['piece'].get_position()[0]
+        last_position_y = last_clicked['piece'].get_position()[1]
+        new_position_x = 0
+        new_position_y = 0
+        new_piece = None
 
+        if isinstance(new_click['piece'], Empty):
+            new_position_x = new_click['piece'].get_position()[0]
+            new_position_y = new_click['piece'].get_position()[1]
+            new_piece = new_click['piece']
+        else:
+            new_position_x = new_click['piece']['piece'].get_position()[0]
+            new_position_y = new_click['piece']['piece'].get_position()[1]
+            new_piece = new_click['piece']['piece']
+        
         # Change position of object
-        temp_pos = last_clicked['piece'].get_position()
-        last_clicked['piece'].set_position(new_click['piece'].get_position()[0], new_click['piece'].get_position()[1])
-        new_click['piece'].set_position(temp_pos[0], temp_pos[1])
+        # temp_pos = last_clicked['piece'].get_position()
+        last_clicked['piece'].set_position(new_position_x, new_position_y)
+        new_piece.set_position(last_position_x, last_position_y)
 
         # Change square color
-        temp_color = self.board[new_click['piece'].get_position()[0]][new_click['piece'].get_position()[1]]['color']
-        self.board[new_click['piece'].get_position()[0]][new_click['piece'].get_position()[1]]['color'] = self.board[last_clicked['piece'].get_position()[0]][last_clicked['piece'].get_position()[1]]['color']
-        self.board[last_clicked['piece'].get_position()[0]][last_clicked['piece'].get_position()[1]]['color'] = temp_color
+        temp_color = self.board[new_position_x][new_position_y]['color']
+        self.board[new_position_x][new_position_y]['color'] = self.board[last_position_x][last_position_y]['color']
+        self.board[last_position_x][last_position_y]['color'] = temp_color
 
         # Change position in board array
-        temp_pos = self.board[last_clicked['piece'].get_position()[0]][last_clicked['piece'].get_position()[1]]
-        self.board[last_clicked['piece'].get_position()[0]][last_clicked['piece'].get_position()[1]] = self.board[new_click['piece'].get_position()[0]][new_click['piece'].get_position()[1]]
-        self.board[new_click['piece'].get_position()[0]][new_click['piece'].get_position()[1]] = temp_pos
+        temp_pos = self.board[last_position_x][last_position_y]
+        self.board[last_position_x][last_position_y] = self.board[new_position_x][new_position_y]
+        self.board[new_position_x][new_position_y] = temp_pos
 
+        # Delete piece and replace with Empty
+        if not isinstance(new_click['piece'], Empty):
+            scrapped_piece = self.board[new_position_x][new_position_y]['piece']
+
+            # Remove from player's pieces list
+            if player.get_name() == 'Player 1':
+                p2.pieces.pop(scrapped_piece['piece'].get_index())
+            else:
+                p1.pieces.pop(scrapped_piece['piece'].get_index())
+            
+            # Remove piece from board
+            self.board[last_position_x][last_position_y]['piece'] = Empty()
+            self.board[last_position_x][last_position_y]['piece'].set_position(last_position_x, last_position_y)
+            
+    # def eat_piece(self, turn, last_clicked, temp_clicked, p1, p2):
     
     def draw_board(self, screen, text_turn, text_total_turns):
         """Draw the chess board"""
         offset = 10
         padding = 65
-        x = padding
-        y = padding + offset
+        x = padding + offset
+        y = padding + offset * 3
 
         # Display text
-        screen.blit(text_turn, (offset, offset))
-        screen.blit(text_total_turns, (475, offset))
+        screen.blit(text_turn, (offset * 4, offset))
+        screen.blit(text_total_turns, (450, offset))
         pygame.display.update()
 
         # Draw the board
@@ -117,15 +144,15 @@ class Board:
                 screen.blit(square, n['rect_pos'])
                 x += 70
             y += 70
-            x = padding
+            x = padding + offset
         pygame.display.update()
 
     def draw_pieces(self, screen, text_turn, text_total_turns):
         """Draw the pieces in their initial positions"""
         offset = 10
         padding = 65
-        x = padding
-        y = padding + offset
+        x = padding + offset
+        y = padding + offset * 3
 
         self.draw_board(screen, text_turn, text_total_turns)
         # Draw the pieces
@@ -138,9 +165,8 @@ class Board:
                     screen.blit(piece, p['piece']['rect_pos'])
                 x += 70
             y += 70
-            x = padding
-        pygame.display.update()
-        
+            x = padding + offset
+        pygame.display.update()        
     
     def update(self, screen, text_turn, text_total_turns):
         """Update the board and pieces"""
@@ -153,6 +179,16 @@ class Board:
                     screen.blit(piece, p['piece']['rect_pos'])
         
         pygame.display.update()
-
-        
-
+    
+    def check_piece_promotion(self, piece, p1, p2, screen, text_turn, text_total_turns, row, color):
+        """Check if any piece in row can be promoted"""
+        col = 0
+        for piece in self.board[row]:
+            if not isinstance(piece['piece'], Empty):
+                if piece['piece']['piece'].get_color() == color:
+                    if p1.get_piece_color() == color and not isinstance(piece['piece']['piece'], Queen):
+                        p1.promote_piece(self, piece, row, col)
+                    elif p2.get_piece_color() == color and not isinstance(piece['piece']['piece'], Queen):
+                        p2.promote_piece(self, piece, row, col)
+                    self.update(screen, text_turn, text_total_turns)
+            col += 1
