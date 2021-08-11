b745425a79a3c38649653f681feec8410de648f4
Omar N <54334118+omnz@users.noreply.github.com>
2021-08-11 17:44:23 -0400

    Update chess.py

--- chess.py
+++ chess.py
@@ -1,5 +1,6 @@
 from Pieces.empty import Empty
 import pygame
+import game
 from pygame.constants import KEYDOWN, QUIT
 from board import Board
 from player import Player
@@ -9,50 +10,10 @@ from player import Player
 p1 = Player('Player 1')
 p2 = Player('Player 2')
 board = Board()
-
 turn_order = []
-winner = ''
 
-# Ask first player for piece color
-while True:
-    color = input(f"{p1.get_name()}, which color piece are you (white/black)? ")
-    if color[0].lower() == 'w':
-        p1.set_piece_color('white')
-        p2.set_piece_color('black')
-
-        print(f"{p1.get_name()}, your piece color is {p1.get_piece_color()}.")
-        print(f"{p2.get_name()}, your piece color is {p2.get_piece_color()}.")
-        break
-    elif color[0].lower() == 'b':
-        p1.set_piece_color('black')
-        p2.set_piece_color('white')
-
-        print(f"{p1.get_name()}, your piece color is {p1.get_piece_color()}.")
-        print(f"{p2.get_name()}, your piece color is {p2.get_piece_color()}.")
-        break
-    else:
-        print("Error: Did not enter white or black!\n")
-
-print()
-
-# Setup the board
-if(p1.get_piece_color() == 'white'):
-    p1.build_pieces('white')
-    p2.build_pieces('black')
-    board.setup(p1, p2)
-    p1.turn()
-    
-
-    turn_order.append(p1)
-    turn_order.append(p2)
-else:
-    p1.build_pieces('black')
-    p2.build_pieces('white')
-    board.setup(p2, p1)
-    p2.turn()
-    
-    turn_order.append(p2)
-    turn_order.append(p1)
+game.get_player_colors(p1, p2)
+game.setup(board, p1, p2, turn_order)
 
 # GAME
 pygame.init()
@@ -61,25 +22,16 @@ icon = pygame.image.load('Sprites/king_black.png')
 pygame.display.set_icon(icon)
 size = 650, 650
 screen = pygame.display.set_mode(size)
+text_details = {
+    'text_turn': '',
+    'text_total_turns': '',
+    'white': (255, 255, 255),
+    'black': (0, 0, 0),
+    'font': pygame.font.Font('freesansbold.ttf', 20),
+    'total_turns': 0
+}
 
-# Text
-white = (255, 255, 255)
-black = (0, 0, 0)
-font = pygame.font.Font('freesansbold.ttf', 20)
-text_turn = ''
-text_total_turns = ''
-total_turns = 0
-
-# Display 1st turn
-if p1.get_piece_color() == 'white':
-    text_turn = font.render(f'Turn: {p1.get_name()} ({p1.get_piece_color().title()})', True, white, black)
-else:
-    text_turn = font.render(f'Turn: {p2.get_name()} ({p2.get_piece_color().title()})', True, white, black)
-
-text_total_turns = font.render(f'Total turns: {total_turns}', True, white, black)
-
-# board.draw(screen, text_turn, text_total_turns)
-board.draw_pieces(screen, text_turn, text_total_turns)
+board.draw_pieces(screen, p1, p2, text_details)
 
 running = True
 last_clicked = None
@@ -120,34 +72,26 @@ while running:
                             # Clicked on possible position
                             if temp_clicked in possible_pos:
                                 board.move_piece(turn, last_clicked, temp_clicked, p1, p2)
-                                board.update(screen, text_turn, text_total_turns)
+                                board.update(screen, p1, p2, text_details)
 
                                 # Check if piece can be promoted
-                                board.check_piece_promotion(last_clicked, p1, p2, screen, text_turn, text_total_turns, 0, 'white')
-                                board.check_piece_promotion(last_clicked, p1, p2, screen, text_turn, text_total_turns, board.rows - 1, 'black')
+                                board.check_piece_promotion(last_clicked, p1, p2, screen, text_details, 0, 'white')
+                                board.check_piece_promotion(last_clicked, p1, p2, screen, text_details, board.rows - 1, 'black')
 
                                 temp_clicked = None
                                 last_clicked = None
                                 
-
                             # Clicked on non-possible position
                             else:
                                 clicked_pos = pygame.mouse.get_pos()
+
+                                # Clicked on a piece
                                 if p['rect_pos'].collidepoint(clicked_pos) and not isinstance(p['piece'], Empty):
                                     if p['piece']['piece'].can_move:
                                         last_clicked = p['piece']
                                         possible_pos = last_clicked['piece'].check_position(board)
                                 temp_clicked = None
                                 
-
-
-  
     pygame.display.update()
 
 pygame.quit()
-
-#     for player in turn_order:
-#         while True:
-#             piece_to_move = input("Enter coordinates of piece you wish to move: ")
-# print(p1.pieces[0]['piece'].get_position())
-# print(p1.pieces[0]['piece'].check_position(board, (5, 0)))
