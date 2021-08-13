a260e0226935b9f1eca270ae542c9c16575e2218
Omar N <54334118+omnz@users.noreply.github.com>
2021-08-13 12:21:45 -0400

    Update knight.py

--- Pieces/knight.py
+++ Pieces/knight.py
@@ -10,7 +10,7 @@ class Knight(Piece):
         super().__init__()
         self.display = 'N'
     
-    def check_position(self, board):
+    def check_position(self, board, player, p1):
         """Returns '1' if position is valid."""
         
         possible_positions = []
