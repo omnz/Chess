86a3644f4bbc315b226ccf865c8c505672b93a2e
Omar N <54334118+omnz@users.noreply.github.com>
2021-08-19 11:32:23 -0400

    Update chess.py

--- chess.py
+++ chess.py
@@ -21,7 +21,9 @@ pygame.init()
 pygame.display.set_caption('Chess')
 icon = pygame.image.load('Sprites/king_black.png')
 pygame.display.set_icon(icon)
-size = 650, 650
+SCREEN_WIDTH = 650
+SCREEN_HEIGHT = 650
+size = SCREEN_WIDTH, SCREEN_HEIGHT
 screen = pygame.display.set_mode(size)
 surface = pygame.Surface(size, pygame.SRCALPHA)
 text_details = {
@@ -34,6 +36,9 @@ text_details = {
 }
 
 running = True
+game_draw = False
+game_over = False
+game_over_count = 0
 possible_pos = []
 current_player = turn_order[0]
 clicks = []
@@ -46,8 +51,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
-        # W.I.P.: Turns
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and running:
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and running and game_over == False:
             mouse_click = pygame.mouse.get_pos()
                 
             if len(clicks) < 2:
@@ -57,7 +61,7 @@ while running:
 
                         # If clicked square is empty
                         if clicked_square and isinstance(p['piece'], Empty):
-                            print(p['piece'])
+                            # print(p['piece'])
                             possible_pos = p['piece'].check_position(board, current_player, p1)
                             game.show_possible_moves(possible_pos, screen, surface, board, current_player, p1)
                             board.update(screen, current_player, text_details)
@@ -82,7 +86,6 @@ while running:
 
                             # Select one of your pieces
                             if current_player.is_turn and p['piece']['piece'].get_color() == current_player.get_piece_color():
-                            # if p['piece']['piece'].can_move:
                                 last_clicked = p
 
                                 # Select first piece
@@ -119,14 +122,23 @@ while running:
                 game.remove_en_passant(p1, p2)
 
                 # Check if piece can be promoted
-                board.check_piece_promotion(clicks[0]['piece'], current_player, screen, text_details, 0, p1)                
+                board.check_piece_promotion(clicks[0]['piece'], current_player, screen, text_details, 0, p1)
 
                 # Get next player and update game
                 game.empty_list(clicks)
                 current_player = game.next_turn(current_player, turn_order, p1, p2)
                 text_details['total_turns'] += 1
-                board.update(screen, current_player, text_details)                      
-                                
+                board.update(screen, current_player, text_details)
+
+                # Check for game over
+                game_over = game.check_mate(current_player, p1, p2)
+
+                # Check for draw (W.I.P.)
+    
+        if game_over and game_over_count == 0:
+            game.game_over(current_player, p1, p2, text_details, screen, size, game_draw)
+            game_over_count += 1
+
     pygame.display.update()
 
 pygame.quit()
